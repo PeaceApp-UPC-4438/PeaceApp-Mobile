b083eb372fa8fee19bf5cb9f3b9a3381dc5e2ea4
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-09-22 19:55:35 -0500

    chore:add UserLogActivity and testing section

---
+++ app/src/main/java/com/innovatech/peaceapp/UserLogActivity.kt
@@ -0,0 +1,11 @@
+package com.innovatech.peaceapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserLogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_log)
+    }
+}
