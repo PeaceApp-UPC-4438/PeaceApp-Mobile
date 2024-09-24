f504eb77697feddc682b8dd0194f086407df8329
Arian Rodriguez <arianmrv12@gmail.com>
2024-09-24 02:47:40 -0500

    update: redirecting to the map view

--- app/src/main/java/com/innovatech/peaceapp/MainActivity.kt
+++ app/src/main/java/com/innovatech/peaceapp/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.innovatech.peaceapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.innovatech.peaceapp.Map.MapActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val btn = findViewById<Button>(R.id.btnPrueba)
+
+        btn.setOnClickListener {
+            val intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
         }
     }
 }
